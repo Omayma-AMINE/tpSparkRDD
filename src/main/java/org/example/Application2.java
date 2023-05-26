686348e43dd0245ccf90e9a9db5a004a9e8ccd85
omayma <omayma@Ubuntu.myguest.virtualbox.org>
2023-05-26 22:39:00 +0100

    COMMIT

--- src/main/java/org/example/Application2.java
+++ src/main/java/org/example/Application2.java
@@ -4,16 +4,19 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.w3c.dom.stylesheets.LinkStyle;
 import scala.Tuple2;
+
+import java.util.List;
 
 public class Application2 {
 
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("TotalVentesParVille").setMaster("local");
+        SparkConf conf = new SparkConf().setAppName("TotalVentesParVille");
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> rddVentes = sc.textFile("ventes.txt");
+        JavaRDD<String> rddVentes = sc.textFile("hdfs://localhost:9000/ventes.txt");
         JavaPairRDD<String,Double> rddVentesVille = rddVentes.mapToPair(line->{
             String[] ventes = line.toString().split(" ");
             String ville = ventes[1];
@@ -23,11 +26,23 @@ public class Application2 {
             return new Tuple2<>((ville+','+year),prix);
         });
 
+        System.out.println("cas1"+rddVentes);
+        System.out.println("cas2"+rddVentesVille);
+
+
 
         JavaPairRDD<String,Double> rddVentesVilleTotal = rddVentesVille.reduceByKey((aDouble, aDouble2) -> aDouble +aDouble2);
-        JavaRDD<String> rddTotal = rddVentesVilleTotal.map(tuple-> tuple._1 +" "+ tuple._2.toString());
 
-        rddTotal.saveAsTextFile("hdfs://localhost:9000/TotalVentesParVilleAnneeSPARK");
+        List<String> rddTotal = rddVentesVilleTotal.map(tuple-> tuple._1 +" "+ tuple._2.toString()).collect();
+
+        //rddTotal.saveAsTextFile("TotalVentesParVilleAnnee");
+
+        for ( String rs : rddTotal) {
+            System.out.println(rs);
+        }
+
+
+
     }
 
 }
